427d7ea1e4608ca7fec79c92b7da5df291eb1931
Kalinuskins_R <kalinuskins.r@gmail.com>
2017-12-20 19:20:57 +0000

    created smarter calculator

---
+++ Calculator/ClapFap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+   public int Calculations
+{
+
+    class Calculations
+{
+    public int AskUserForNumber()
+
+
+        Console.WriteLine("please enter number");
+        //ielasit ciparu no konsoles
+        int number;
+    //ieskaita 
+
+    bool success = Int32.TryParse(inputText, out number);
+    
+      if (sucess == false)
+      {
+        Console.WriteLine("Sorry wrong value entered");
+        number = AskUserForNumber();
+      }
+
+    return number;
+	}
+}
+]
---
+++ Calculator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorld
+
+class PersonGreeter
+{
+    public void SayHello();
+	{
+        string myName;
+        myName = Console.Readline();
+        Console.Writeline(HelloText + myName);
+    }
+}
