b62c49897d4a91e4ea70e45351c328fa335fcda4
Kalinuskins_R <kalinuskins.r@gmail.com>
2017-12-20 17:06:20 +0000

    added a sea style hello

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class PersonGreeter
+
+    { static void Main(String[] args)
+    
+
+
+    PersonGreeter greet;
+        greet = new PersonGreeter();
+        greet.HelloWorld = "Hello World";
+    greet.SayHello();
+
+    PersonGreeter seaGreet;
+        seaGreet = new PersonGreeter
+        seaGreet.HelloText = "Ahoy World";
+        seaGreet.SayHello();
+    
+
+    Console.ReadLine();
+            }
+    }
+}
